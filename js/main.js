13376ec9bd5e0347f5546542b5aa15803876aa38
Jahidul Hasan <jahidulhasanhemal@gmail.com>
2021-06-28 01:26:31 +0600

    Updated skills and skills css and js

--- js/main.js
+++ js/main.js
@@ -51,6 +51,22 @@ skillsHeader.forEach((el) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target);
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification-active');
+        })
+        target.classList.add('qualification-active');
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification-active');
+        })
+        tab.classList.add('qualification-active');
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
